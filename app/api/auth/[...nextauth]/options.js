bc54f4b123df6adf7337a4ae86d7620787616619
Liya Daniel <luhygenet@gmail.com>
2025-09-30 14:06:16 +0300

    added github and google providers

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,2 +1,51 @@
-import GitHubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+  provider: [
+    GitHubProvider({
+      profile(profile) {
+        console.log("Profile Github: ", profile);
+
+        let userRole = "Github User";
+        if (profile?.email == "liya.daniel@a2sv.org") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+
+    GoogleProvider({
+      profile(profile) {
+        console.log("Profile Google: ", profile);
+
+        return {
+          ...profile,
+          id: profile.sub,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+
+  callback: {
+    //server side
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    //client side
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
