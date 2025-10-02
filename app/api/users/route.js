53647545dff111c4b337e3cc67af91d42a23150a
Liya Daniel <luhygenet@gmail.com>
2025-10-02 18:34:09 +0300

    added db and api for CRUD functionality to it

---
+++ app/api/users/route.js
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import User from "../../(models)/User";
+import bcrypt from "bcrypt";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const userData = body.formData;
+
+    //confirm user actually typed sth
+    if (!userData?.email || !userData.password) {
+      return NextResponse.json(
+        {
+          message: "All fields are required",
+        },
+        { status: 400 }
+      );
+    }
+
+    const duplicate = await User.findOne({ email: userData.email })
+      .lean()
+      .exec();
+
+    if (duplicate) {
+      return NextResponse.json({ message: "Duplicate Email" }, { status: 409 });
+    }
+
+    const hashPassword = await bcrypt.hash(userData.password, 10);
+    userData.password = hashPassword;
+
+    await User.create(userData);
+    console.log("Created user:", userData);
+    return NextResponse.json({ message: "User Created" }, { status: 201 });
+    //or
+
+    // if (!userData || Object.keys(userData).length === 0) {
+    //     return NextResponse.json({ message: "No user data provided" }, { status: 400 })
+    // }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
