dc58b943b5b934bd1333621d91d612fa05b51e05
Liya Daniel <luhygenet@gmail.com>
2025-10-14 10:10:36 +0300

    added simpler middleware check for member page

--- app/Member/page.jsx
+++ app/Member/page.jsx
@@ -5,15 +5,12 @@ import { options } from "../api/auth/[...nextauth]/options";
 
 const Member = async () => {
   const session = await getServerSession(options);
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/Member");
-  }
+ 
   return (
     <div>
       <h1>Member Server Session</h1>
       <div>{}</div>
-      <div>{session?.user?.email}</div>
-      <div>{session?.user?.role}</div>
+     
     </div>
   );
 };
--- middleware.js
+++ middleware.js
@@ -4,26 +4,26 @@ import { NextResponse } from "next/server";
 // really simple way to protect a route from users not logged in
 // export { default } from "next-auth/middleware";
 
-export default withAuth(function middleware(req) {
-  console.log(req.nextUrl.pathname);
-  console.log(req.nextauth.token.role);
+export default withAuth(
+  function middleware(req) {
+    console.log(req.nextUrl.pathname);
+    console.log(req.nextauth.token.role);
 
-  if (
-    req.nextUrl.pathname.startsWith("/CreateUser") &&
-    req.nextauth.token.role != "admin"
-  ) {
-    return NextResponse.rewrite(new URL("/Denied", req.url));
-    //added the withauth comment
-  }
-
-},
-{
-    callbacks:{
-        authorized:({token}) => !!token
+    if (
+      req.nextUrl.pathname.startsWith("/CreateUser") &&
+      req.nextauth.token.role != "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/Denied", req.url));
+      //added the withauth comment
     }
-}
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
 );
 
 export const config = {
-  matcher: ["/CreateUser"],
+  matcher: ["/CreateUser", "/Member"],
 };
